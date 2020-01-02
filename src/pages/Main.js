ad2067ddfdfd49d90a78c3ffc85268c367cb4b31
iaan <didgur0601@gmail.com>
2020-01-02 16:49:27 +0900

    window.scroll to top으로 가는 기능 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -137,6 +137,44 @@ const Image = styled.div`
   margin-bottom: 2px;
 `;
 
+const TopButton = styled.button`
+  &:focus,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  outline-style: none;
+
+  position: fixed;
+  bottom: 150px;
+  right: 0;
+  margin-right: 70px;
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  border: solid 1px #e84393;
+  cursor: pointer;
+  background-color: #e84393;
+  color: #bdc3c7;
+
+  &:hover {
+    background-color: #fd79a8;
+    border: solid 1px #fd79a8;
+    color: #ecf0f1;
+  }
+
+  -webkit-box-shadow: 0 2px 4px rgba(0, 0, 0, 0.6);
+  -moz-box-shadow: 0 2px 4px rgba(0, 0, 0, 0.6);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.6);
+  -webkit-background-clip: padding-box;
+  -moz-background-clip: padding-box;
+  background-clip: padding-box;
+`;
+
 const Main = props => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
@@ -214,11 +252,11 @@ const Main = props => {
               </>
             )}
           </ContentBox>
-          {/* <TopButton
+          <TopButton
             onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
           >
             top
-          </TopButton> */}
+          </TopButton>
         </Column>
       </Row>
     </Container>
